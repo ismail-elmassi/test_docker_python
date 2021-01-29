bc2540209883a26ce8d7b1c5f787d9f38fbd4214
ismail-elmassi <ismail.elmassi@gmail.com>
2021-01-29 05:41:27 -0800

    add changes to a file

--- index.py
+++ index.py
@@ -4,4 +4,5 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int("5000"), debug=True)
+	print("**********hello world**********")
+	app.run(host="0.0.0.0", port=int("5000"), debug=True)
